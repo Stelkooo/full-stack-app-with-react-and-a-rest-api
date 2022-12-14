da47cb3684e830031136baca436e83c92ea50891
stelko <stilyanmtoskov@gmail.com>
2022-12-14 11:37:10 +0000

    UserSignUp component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from './components/Header';
+import UserSignUp from './components/UserSignUp';
 import CreateCourse from './components/CreateCourse';
 import UpdateCourse from './components/UpdateCourse';
 
---
+++ client/src/components/UserSignUp.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export default class UserSignUp extends React.Component {
+  render() {
+    return (
+      <main>
+        <div className='form--centered'>
+          <h2>Sign Up</h2>
+          <form>
+            <label for='firstName'>First Name</label>
+            <input
+              id='firstName'
+              name='firstName'
+              type='text'
+            />
+            <label for='lastName'>Last Name</label>
+            <input
+              id='lastName'
+              name='lastName'
+              type='text'
+            />
+            <label for='emailAddress'>Email Address</label>
+            <input
+              id='emailAddress'
+              name='emailAddress'
+              type='email'
+            />
+            <label for='password'>Password</label>
+            <input
+              id='password'
+              name='password'
+              type='password'
+            />
+            <button
+              className='button'
+              type='submit'
+            >
+              Sign Up
+            </button>
+            <button className='button'>Cancel</button>
+          </form>
+          <p>
+            Already have a user account? Click here to <a>sign in</a>!
+          </p>
+        </div>
+      </main>
+    );
+  }
+}
