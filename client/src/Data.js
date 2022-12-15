2b69ad3b0fa92527c1b561455e3b312ec94377d3
stelko <stilyanmtoskov@gmail.com>
2022-12-15 00:30:45 +0000

    get courses from api

---
+++ client/src/Data.js
@@ -0,0 +1,44 @@
+import apiBaseUrl from './config';
+
+export default class Data {
+  api(
+    path,
+    method = 'GET',
+    body = null,
+    requiresAuth = false,
+    credentials = null,
+  ) {
+    const url = apiBaseUrl + path;
+
+    const options = {
+      method,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+    };
+
+    if (body !== null) {
+      options.body = JSON.stringify(body);
+    }
+
+    if (requiresAuth) {
+      const encodedCredentials = btoa(
+        `${credentials.username}:${credentials.password}`,
+      );
+
+      options.headers['Authorization'] = `Basic ${encodedCredentials}`;
+    }
+
+    return fetch(url, options);
+  }
+
+  async getCourses() {
+    const response = await this.api('/courses', 'GET', null, false, null);
+
+    if (response.status === 200) {
+      return response.json().then((data) => data);
+    } else {
+      throw new Error();
+    }
+  }
+}
